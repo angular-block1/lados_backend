4815da541f2a8d550a13640c61f2aacc862cdfb2
hiepblack <chuongduong0123@gmail.com>
2023-06-04 20:51:06 +0700

    LB-1 setup env

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import product from "./Router/Product.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+app.get("/", (req, res) => {
+  res.send("api working");
+});
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("mongodb connect successful");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// middleware
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use("/product", product);
+
+app.listen(port, () => {
+  connect();
+  console.log("server listening on port", port);
+});
