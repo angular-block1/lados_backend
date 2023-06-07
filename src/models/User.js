ec41d2282bbadbe8b106df3ada11ac9a96235b59
Duong Van Toan <toandvph19458@fpt.edu.vn>
2023-06-07 21:15:38 +0700

    feat: middlewareAuth refs LB-26-middleware-auth

--- src/models/User.js
+++ src/models/User.js
@@ -1,31 +1,36 @@
-import { Schema, model } from "mongoose"
-
+import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-	name: {
-		type: String,
-	},
-	email: {
-		type: String,
-	},
-	password: {
-		type: String,
-	},
-	orders: {
-		type: [{
-			type: Schema.Types.ObjectId,
-			ref: 'Order'
-		}],
-		default: []
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-	updatedAt: {
-		type: Date,
-		default: Date.now,
-	},
-})
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  orders: {
+    type: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
+    default: [],
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-export default model('User', userSchema)
+export default model("User", userSchema);
