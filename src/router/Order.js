90efb29162138c5daf4eb4bb8ec774194e408c20
ducph19546 <105204373+ducph19546@users.noreply.github.com>
2023-06-09 17:23:38 +0700

    Lb 7 them sản phẩm (#18)
    * feat: controller crud sản phẩm
    refs LB-3
    
    * feat: middlewareAuth refs LB-26-middleware-auth (#8)
    
    Co-authored-by: Duong Van Toan <toandvph19458@fpt.edu.vn>
    
    * feat: auth signin signup refs LB-5-controller-user (#9)
    
    Co-authored-by: Duong Van Toan <toandvph19458@fpt.edu.vn>
    
    * feat: api đơn hàng refs LB-6 (#11)
    
    * LB-12 addCategory
    
    * LB-14 removecate
    
    * fix: sửa route
    refs LB-3
    
    * fix: category controller
    refs LB-7
    
    * fix: merge LB-7 into dev
    refs LB-7
    
    ---------
    
    Co-authored-by: ducnguyen1002 <ducduc1022000@gmail.com>
    Co-authored-by: toandvph19458 <105204332+toandvph19458@users.noreply.github.com>
    Co-authored-by: Duong Van Toan <toandvph19458@fpt.edu.vn>
    Co-authored-by: Trường Nguyễn <97250708+truongnq0509@users.noreply.github.com>
    Co-authored-by: hiepblack <chuongduong0123@gmail.com>

--- src/router/Order.js
+++ src/router/Order.js
@@ -1,4 +1,23 @@
 import express from "express";
+<<<<<<< HEAD
+import { get, create, cancelOrder, getOrderByUser, payMomo, payVnPay, updateOrder, updateOrderStatus } from "../controllers/Order.js"
+import { verifyAccessToken } from "../middlewares/middlewareAuth.js"
+
+const router = express.Router()
+
+router.use(verifyAccessToken)
+router.get("/me", getOrderByUser)
+router.post('/', create)
+router.get("/:id", get)
+router.get("/", get)
+router.post("/pay-momo", payMomo)
+router.post("/pay-vnpay", payVnPay)
+router.put("/:id", updateOrder)
+router.delete("/:id", cancelOrder)
+router.patch("/:id", updateOrderStatus)
+
+export default router
+=======
 import {
   get,
   create,
@@ -26,3 +45,4 @@ router.delete("/:id", cancelOrder);
 router.patch("/:id", updateOrderStatus);
 
 export default router;
+>>>>>>> e32c446735400e4f5e7abb4ef4b3ce89b601f6e9
