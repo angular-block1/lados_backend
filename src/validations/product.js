3f1eea76681c2068d83f916559d4448c081298cc
ducnguyen1002 <ducduc1022000@gmail.com>
2023-06-09 20:09:29 +0700

    fix: sửa Schema product

--- src/validations/product.js
+++ src/validations/product.js
@@ -1,12 +1,16 @@
 import joi from "joi";
 
 const productShema = joi.object({
+  _id: joi.string(),
+  createdAt: joi.string(),
+  updatedAt: joi.string(),
+  __v: joi.number(),
   name: joi.string().min(6).required(),
   slug: joi.string().required(),
   price: joi.number().min(0).required(),
   stock: joi.number().min(0).required(),
   images: joi.array().required(),
-  category: joi.string().required(),
+  category: joi.any().required(),
   description: joi.string().required(),
   status: joi.boolean()
 });
